74e9ca3894b10f241f0385681ffe457da521aedd
Alanis Yates <yatesalanis@gmail.com>
2022-08-21 14:38:30 -0400

    import files nostyle

--- bank_tailwind_tut/src/App.jsx
+++ bank_tailwind_tut/src/App.jsx
@@ -1,20 +1,46 @@
 import React from "react";
+import {
+  Hero,
+  Navbar,
+  Billing,
+  Button,
+  CTA,
+  Client,
+  CardDeal,
+  FeedbackCard,
+  Footer,
+  GetStarted,
+  Testimonial,
+  Stats,
+  Buisness,
+} from "./components";
 import styles from "./style";
 
 const App = () => {
   return (
     <div className="bg-primary w-full verflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth}`}>NavBar</div>
-      </div>
-
-      <div className={`bg-primary ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>Hero</div>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
       </div>
 
       <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Stats buisness Billing Card Detail Test Clients CTA Footer
+          <Hero />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Buisness />
+          <Billing />
+          <CardDeal />
+          <Testimonial />
+          <Client />
+          <CTA />
+          <Footer />
         </div>
       </div>
     </div>
---
+++ bank_tailwind_tut/src/components/index.js
@@ -0,0 +1,29 @@
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import Button from "./Button";
+import CTA from "./CTA";
+import Client from "./Client";
+import CardDeal from "./CardDeal";
+import FeedbackCard from "./FeedbackCard";
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Testimonial from "./Testimonial";
+import Stats from "./Stats";
+import Buisness from "./Buisness";
+
+export {
+  Hero,
+  Navbar,
+  Billing,
+  Button,
+  CTA,
+  Client,
+  CardDeal,
+  FeedbackCard,
+  Footer,
+  GetStarted,
+  Testimonial,
+  Stats,
+  Buisness,
+};
